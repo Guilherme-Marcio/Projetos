6f905e7f0e7046ef983d584a3622a127a6aa6e12
Guilherme Marcio <guilhermemarcio281@gmail.com>
2026-03-20 18:49:15 -0300

    feat:Adiciona Matriz de falhas em ambientes de teste

---
+++ Matriz de falhas/Matriz de falhas/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrizDeFalhas
+{
+    internal class Program
+    {
+        // Tarefa 3: Preencher Matriz 
+        static void PreencherMatriz(int[,] matriz)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = rnd.Next(0, 21);
+                }
+            }
+        }
+
+        // Tarefa 4: Mostrar Falhas 
+        static void MostrarFalhas(int[,] matriz)
+        {
+            Console.WriteLine("--- Lista de Falhas ---");
+
+            foreach (int falha in matriz)
+            {
+                Console.Write($"| Falha: {falha} ");
+            }
+            Console.WriteLine("|");
+        }
+
+        // Tarefa 5: Calcular Total de Falhas
+        static int CalcularTotalFalhas(int[,] matriz)
+        {
+            int somaTotal = 0;
+
+            foreach (int f in matriz)
+            {
+                somaTotal += f; 
+            }
+
+            return somaTotal;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número de ambientes: ");
+            int amb = int.Parse(Console.ReadLine());
+            Console.Write("Digite os tipos de Testes: ");
+            int testes = int.Parse(Console.ReadLine());
+
+            int[,] matrizFalhas = new int[amb, testes];
+
+            PreencherMatriz(matrizFalhas);
+            MostrarFalhas(matrizFalhas);
+
+            int total = CalcularTotalFalhas(matrizFalhas);
+            Console.WriteLine($"O total de falhas encontradas foi: {total}");
+
+        }
+    }
+}
---
+++ Matriz de falhas/Matriz de falhas/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Matriz de falhas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Matriz de falhas")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("b9c2f031-9cc0-41aa-97a3-e1b7f5f7c179")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
